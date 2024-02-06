a9080147d116df564190b22f195a629e428030a3
Jean-Aime-2023 <124204227+Jean-Aime-2023@users.noreply.github.com>
2024-02-06 20:21:50 +0200

    font family

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,11 @@
 import type { Metadata } from 'next';
+import { Outfit } from 'next/font/google';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { ClerkProvider, SignIn, SignedIn, SignedOut } from '@clerk/nextjs';
 import Navbar from '@/components/Navbar';
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Outfit({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Create Next App',

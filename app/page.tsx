5cec1017e34e3d1f456f1415492948f9dc33fd32
Jean-Aime-2023 <124204227+Jean-Aime-2023@users.noreply.github.com>
2024-02-07 06:14:50 +0200

    hygraph and data fetching

--- app/page.tsx
+++ app/page.tsx
@@ -4,17 +4,20 @@ import CarsFiltersOptions from "@/components/Home/CarsFiltersOptions";
 import Hero from "@/components/Home/Hero";
 import SearchInput from "@/components/Home/SearchInput";
 import { getCarsList } from "@/services";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+
+  const [carsList,setCarsList] = useState<any>([])
   useEffect(() => {
     getCarsList_();
   }, [])
   
   const getCarsList_ = async ()=>{
-    const result = await getCarsList();
-    console.log(result)
+    const result:any = await getCarsList();
+    setCarsList(result?.carsList)
   }
+  
   return (
     <div className="p-5 sm:px-10 md:px-20">
       <Hero/>
@@ -23,7 +26,3 @@ export default function Home() {
     </div>
   );
 }
-
-
-//timestamp
-//59:35
